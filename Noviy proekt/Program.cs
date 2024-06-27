38e4cbc44aba8dad33a5a65c9ddf6d24b16e2dea
IT_Dino 10 <IT_Dino 10@DESKTOP-5JATGAG>
2024-06-27 14:27:39 +0300

    обновлённое задание

--- Noviy proekt/Program.cs
+++ Noviy proekt/Program.cs
@@ -1,19 +1,26 @@
 ﻿
 
 
-String alphabet = "абвгдежзийклмнопрсттуфхцчшщьыъэюя";
+
+char[] alphabet = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
 Console.WriteLine("Введите слово: ");
 String text = Console.ReadLine();
 text = text.ToLower();
 String result = "";
-int s = 0;
+int s = 0, k = 0;
 Console.WriteLine("1 - Зашифровать \n2 - Расшифровать");
 int a = Convert.ToInt32(Console.ReadLine());
 switch (a)
 {
     case 1:
         for (int i = 0; i < text.Length; i++)
-        {
+        {   
+            if (alphabet[i] != text[i])
+            {
+                Console.WriteLine("Неправильно ввёл");
+                break;
+            }
+            
             for (int j = 0; j < alphabet.Length; j++)
             {
                 if (text[i] == alphabet[j])
@@ -25,31 +32,47 @@ switch (a)
             if (i == 0) result += text[i];
             else
             {
-                s = s + text[i - 1];
+                s = s + i - 1;
                 result += alphabet[s % alphabet.Length];
+
             }
         }
         Console.WriteLine("Зашифрованное слово: " + result);
         break;
     case 2:
-        for (int i = 0; i < text.Length; i++)
         {
-            for (int j = 0; j < alphabet.Length; j++)
+            for (int i = 0; i < text.Length; i++)
             {
-                if (text[i] == alphabet[j])
+                if (alphabet[i] != text[i])
                 {
-                    s = j;
+                    Console.WriteLine("Неправильно ввёл");
                     break;
                 }
+ 
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (text[i] == alphabet[j])
+                    {
+                        s = j;
+                        break;
+                    }
+                }
+                if (i == 0) result += text[i];
+                else
+                {
+                    s = s - i - 1 % alphabet.Length;
+                    if (s < 0) s = alphabet.Length + s;
+                    result += alphabet[s];
+
+                }
+
+
+                Console.WriteLine("Расшифрованное слово: " + result);
             }
-            if (i == 0) result += text[i];
-            else
-            {
-                s = s + text[i - 1];
-                result += alphabet[alphabet.Length - (s % alphabet.Length)];
-            }
+            break;
         }
-        Console.WriteLine("Расшифрованное слово: " + result);
-        break;
 }
-        
+
+
+
+
