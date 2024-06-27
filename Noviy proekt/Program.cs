1eadf58f932e5dfeb4a2b282b65b4382ec9c86c1
IT_Dino 10 <IT_Dino 10@DESKTOP-5JATGAG>
2024-06-27 14:04:03 +0300

    Добавьте файлы проекта.

---
+++ Noviy proekt/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+String alphabet = "абвгдежзийклмнопрсттуфхцчшщьыъэюя";
+Console.WriteLine("Введите слово: ");
+String text = Console.ReadLine();
+text = text.ToLower();
+String result = "";
+int s = 0;
+Console.WriteLine("1 - Зашифровать \n2 - Расшифровать");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+        for (int i = 0; i < text.Length; i++)
+        {
+            for (int j = 0; j < alphabet.Length; j++)
+            {
+                if (text[i] == alphabet[j])
+                {
+                    s = j;
+                    break;
+                }
+            }
+            if (i == 0) result += text[i];
+            else
+            {
+                s = s + text[i - 1];
+                result += alphabet[s % alphabet.Length];
+            }
+        }
+        Console.WriteLine("Зашифрованное слово: " + result);
+        break;
+    case 2:
+        for (int i = 0; i < text.Length; i++)
+        {
+            for (int j = 0; j < alphabet.Length; j++)
+            {
+                if (text[i] == alphabet[j])
+                {
+                    s = j;
+                    break;
+                }
+            }
+            if (i == 0) result += text[i];
+            else
+            {
+                s = s + text[i - 1];
+                result += alphabet[alphabet.Length - (s % alphabet.Length)];
+            }
+        }
+        Console.WriteLine("Расшифрованное слово: " + result);
+        break;
+}
+        
